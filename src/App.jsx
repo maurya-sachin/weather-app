35857d256193671e03f2a96a7daa8be1f399cab1
Sachin Maurya <sachin.maurya@kreatetechnologies.com>
2024-12-26 15:53:23 +0530

    installing react-router-dom and implementing routed on component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
+import { LayoutDashboard, Moon, Settings2, Sun } from "lucide-react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./components/dashboard/Dashboard.jsx";
-import { useEffect } from "react";
-import { useState } from "react";
+import Setting from "./components/setting/Setting.jsx";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    // Checking if hava  saved dark mode preference in localStorage
+    // Checking if saved dark mode preference exists in localStorage
     const savedDarkMode = localStorage.getItem("darkMode") === "true";
     setDarkMode(savedDarkMode);
 
@@ -30,28 +31,51 @@ function App() {
   }, [darkMode]);
 
   return (
-    <div
-      className={`min-h-screen transition-colors duration-200  dark:bg-gray-900 dark:text-gray-100`}
-    >
-      <div className="container mx-auto px-4">
-        <div className="flex justify-between items-center py-4">
-          <h1 className="text-3xl font-bold text-blue-500">
-            Weather Dashboard
-          </h1>
-          <button
-            onClick={() => setDarkMode((prev) => !prev)}
-            className="p-2 rounded-lg bg-white/10 backdrop-blur-md border border-white/20"
-          >
-            {darkMode ? (
-              <Sun className="w-6 h-6" />
-            ) : (
-              <Moon className="w-6 h-6" />
-            )}
-          </button>
+    <Router>
+      <div
+        className={`min-h-screen transition-colors duration-200 ${
+          darkMode
+            ? "bg-gradient-to-br from-gray-900 via-gray-800 to-black dark:text-gray-100"
+            : "bg-gradient-to-br from-blue-100 via-white to-blue-300 text-gray-800"
+        }`}
+      >
+        <div className="container mx-auto px-4">
+          <div className="flex justify-between items-center py-4 relative">
+            <h1 className="text-3xl font-bold text-blue-500">
+              Weather Dashboard
+            </h1>
+            <div className="controls flex flex-col gap-2 absolute right-0 top-5">
+              <button
+                className="p-2 rounded-lg bg-white/10 backdrop-blur-md border border-white/20"
+                onClick={() => setDarkMode((prev) => !prev)}
+              >
+                {darkMode ? (
+                  <Sun className="w-6 h-6" />
+                ) : (
+                  <Moon className="w-6 h-6" />
+                )}
+              </button>
+              <Link
+                to="/"
+                className="p-2 rounded-lg bg-white/10 backdrop-blur-md border border-white/20"
+              >
+                <LayoutDashboard className="w-6 h-6" />
+              </Link>
+              <Link
+                to="/setting"
+                className="p-2 rounded-lg bg-white/10 backdrop-blur-md border border-white/20"
+              >
+                <Settings2 className="w-6 h-6" />
+              </Link>
+            </div>
+          </div>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/setting" element={<Setting />} />
+          </Routes>
         </div>
-        <Dashboard />
       </div>
-    </div>
+    </Router>
   );
 }
 
